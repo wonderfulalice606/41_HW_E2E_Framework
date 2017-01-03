f0b9f72401f49b876e850513fa4ce36d83853d6d
Alice Wonderful <wonderfulalice606@gmail.com>
2017-01-02 16:50:57 -0800

    E2E Framework

---
+++ src/main/java/core/MyHtmlUnit.java
@@ -0,0 +1,72 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class MyHtmlUnit {
+public String[][] a2dim() throws IOException {
+	
+	String csvFile = "./src/main/resources/My_Title_Validation.csv";
+			BufferedReader br = null;
+	String line = null;
+	String[] column = null;
+	int lines = 0;
+	int columns = 0;
+	String SplitBy = "@";
+	String testcase_id = null;
+	String url_address = null;
+	String expected_title = null;
+	
+	//counting lines and columns
+	
+	
+	
+	br = new BufferedReader(new FileReader(csvFile));
+	while ((line = br.readLine()) !=null) {
+		lines++;
+		column = line.split(SplitBy);
+		columns = column.length;
+	}
+	br.close();
+	
+	String s2dim[][] = new String[lines][columns];
+	br = new BufferedReader(new FileReader(csvFile));
+	// WebDriver driver = new FirefoxDriver();
+	WebDriver driver = new HtmlUnitDriver();
+	int i = 0;
+	while ((line = br.readLine()) !=null) {
+		
+		String[] csv = line.split(SplitBy);
+		
+		testcase_id = csv[0];
+		url_address = csv[1];
+		expected_title = csv[2];
+		
+		driver.get(url_address);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		String actual_title = driver.getTitle();
+		
+		s2dim[i][0] = testcase_id;
+		s2dim[i][1] = expected_title;
+		s2dim[i][2] = actual_title;
+		i++;
+	}
+	
+	driver.quit();
+	br.close();
+	return s2dim;
+}
+
+//	public static void main(String[] args) throws IOException {
+//		core.MyHtmlUnit selenium = new core.MyHtmlUnit();
+//		selenium.a2dim();
+//	}
+
+}
